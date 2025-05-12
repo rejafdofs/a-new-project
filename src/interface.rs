ba332e565f662904c2e37d111f57738662052b8d
rejafdofs <rejafdofs@kawaii.blue>
2025-05-13 08:11:57 +0900

    a

---
+++ src/interface.rs
@@ -0,0 +1,5 @@
+
+#[unsafe(no_mangle)]
+pub extern "C" fn load(h: HGLOBAL, len: i32) -> i32 {
+    0
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 mod test;
+mod parser;
+pub mod interface;
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
