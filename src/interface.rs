ec97d06432195620c2343d22625cf7c0552fcffa
rejafdofs <rejafdofs@kawaii.blue>
2025-05-13 20:29:36 +0900

    a

--- src/interface.rs
+++ src/interface.rs
@@ -1,5 +1,14 @@
+use std::ffi::c_long;
+use ::windows::Win32::Foundation::HGLOBAL;
 
 #[unsafe(no_mangle)]
-pub extern "C" fn load(h: HGLOBAL, len: i32) -> i32 {
-    0
+pub extern "C" fn load(h: HGLOBAL, len: c_long) -> bool {
+    todo!()
+}
+pub extern "C" fn unload() -> bool {
+    todo!()
+}
+pub extern "C" fn request(h: HGLOBAL, len: *const c_long) -> HGLOBAL {
+    let _ = len;
+    todo!()
 }
